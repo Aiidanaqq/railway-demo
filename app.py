28da3476000b423abcf34b6ab345cbcf4133bf41
Aiidanaqq <kokumovaaidanaaa@gmail.com>
2026-04-28 19:09:27 +0600

    init railway demo

---
+++ app.py
@@ -0,0 +1,12 @@
+cat > app.py << 'EOF'
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Привет из Railway!</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+EOF
