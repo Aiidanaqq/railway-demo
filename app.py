83b132c6800dea17f10bfd892c1db0c596a2c212
Aiidanaqq <kokumovaaidanaaa@gmail.com>
2026-04-28 19:35:28 +0600

    fix port

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def home():
     return '<h1>Привет из Railway!</h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-EOF
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
